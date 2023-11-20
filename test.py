8d6b13d6ccb0def46370b32cdbaf1e7c214399a3
marcsilva <marciliodevjava@gmail.com>
2023-11-20 19:14:09 -0300

    Utilizando orientação objeto com python

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ def cria_conta(numero, titular, saldo, limite):
              "limite": limite}
 
     return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -=valor
+
+def extrato(conta):
+    return "Saldo é {}".format(conta["saldo"])
