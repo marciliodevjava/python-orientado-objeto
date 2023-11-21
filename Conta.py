01bbfdbe80ca634ec1b506a5f6ce387249905d57
marcsilva <marciliodevjava@gmail.com>
2023-11-21 15:42:34 -0300

    Update Conta

--- Conta.py
+++ Conta.py
@@ -8,3 +8,10 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def sacar(self, conta, valor):
+        if conta.saldo >= valor:
+            self.saldo -= valor
+            return "Saque realiado de R$:{}".format(float(valor))
+
+        return "Seu Saldo é {}".format(float(conta.saldo))
