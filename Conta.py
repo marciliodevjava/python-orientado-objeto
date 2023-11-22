9f3d2c1128db3c80beaba219ddd164c3fc3444c4
marcsilva <marciliodevjava@gmail.com>
2023-11-22 15:59:06 -0300

    Adicionando o metodo parar_conta

--- Conta.py
+++ Conta.py
@@ -9,6 +9,7 @@ class Conta:
         self.__saldo = float(saldo)
         self.__limite = float(limite)
         self.__data = self.formata_data()
+        self.__valor_devido = float(0.0)
         self.__conta_fechada = False
 
     @property
@@ -38,10 +39,16 @@ class Conta:
         return self.__data
 
     def saca(self, valor):
+        valor_conta = self.__limite + self.__saldo
         if self.__saldo >= float(valor):
             self.__saldo -= float(valor)
             return "Saque realiado de R$:{}".format(float(valor))
-
+        elif valor_conta >= float(valor):
+            valor_restante = valor - self.__saldo
+            self.__saldo = 0
+            self.__limite -= valor_restante
+            self.__valor_devido = valor_restante
+            return "Saque realiado de R$:{}".format(float(valor))
         return "Seu Saldo é {}".format(float(self.__saldo))
 
     def deposita(self, valor):
@@ -54,6 +61,7 @@ class Conta:
         print("Nome: {}".format(self.__titular))
         print("Saldo R$:{}".format(float(self.__saldo)))
         print("Limite de credito R$:{}".format(float(self.__limite)))
+        print(("Valor devido R$: {}".format(self.__valor_devido)))
         print("Data de Abertura de conta {}".format(self.__data))
 
     def transferir(self, conta, valor):
@@ -80,4 +88,17 @@ class Conta:
         self.__data = self.formata_data()
         print("Conta fechada com sucesso.")
 
+    def pagar_conta(self):
+        if self.__saldo >= 1:
+            valor_devido = self.__valor_devido
+            valor_saldo = self.__saldo
+            self.__valor_devido -= self.__saldo
+            self.__limite += self.saldo
+            if self.__saldo <= self.__valor_devido:
+                self.__saldo = float(0.0)
+            else:
+                self.__saldo -= valor_devido
+
+            valor_pago = valor_devido - self.__valor_devido
+            return "Conta paga com sucesso R$: {}".format(float(valor_pago))
 
