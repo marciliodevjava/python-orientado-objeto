c123ec1faeb4fe1ae2c5c52787d06a843a7d71c6
marcsilva <marciliodevjava@gmail.com>
2023-11-22 17:08:57 -0300

    Criando Classe TestConta para validar os metodos

---
+++ TestConta.py
@@ -0,0 +1,16 @@
+from Conta import Conta
+
+
+class TestConta:
+    separado = "============================================"
+    conta = Conta(12, 12, "Marcilio", 100, 1000)
+    conta.extrato()
+    print(separado)
+    conta.saca(800)
+    conta.extrato()
+    print(separado)
+    conta.deposita(1000)
+    conta.extrato()
+    print(separado)
+    conta.pagar_conta()
+    conta.extrato()
